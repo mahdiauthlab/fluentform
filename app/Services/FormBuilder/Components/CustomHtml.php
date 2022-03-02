502be4edc347dcd00f50d0dc7791998bd308de55
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2022-03-02 12:34:54 +0600

    register script for handle show hide text

--- app/Services/FormBuilder/Components/CustomHtml.php
+++ app/Services/FormBuilder/Components/CustomHtml.php
@@ -28,19 +28,51 @@ class CustomHtml extends BaseComponent
 
 		if ($data['settings']['show_as_reveal_text']) {
 			$length = $data['settings']['show_count_length'] ? $data['settings']['show_count_length'] : 50 ;
+			
 			$showText = substr(strip_tags($data['settings']['html_codes']), 0, $length);
 			$showText .=  '...';
+			
 			$elementId = $this->makeElementId($data, $form);
-			$readMoreButton = "<button type='button' id='". $elementId.'_read_more_button' ."'>".$data['settings']['readmore_button_text']."</button>";
-			$readLessButton = "<button type='button' id='". $elementId.'_read_less_button' . "'>" . $data['settings']['readless_button_text']."</button>";
-			$innerHtml = "<p id='" . $elementId.'_text' . "'>" . $showText . $readMoreButton ."</p>";
+
+			$readMoreButton = "<p class='read-more ff_read_more_btn' type='button' id='". $elementId.'_read_more_button' ."'><a href='#'>".$data['settings']['readmore_button_text']."<span>&rarr;</span></a></p>";
 			$allText = $data['settings']['html_codes'];
-		
-		}else{
+
+			$readLessButton = "<p class='ff_read_less_btn'> <a href='#'><span>&larr;</span>" . $data['settings']['readless_button_text']."</a></p>";
+
+			$innerHtml = "<div class='ff_truncated_text'>{$showText} {$readMoreButton}</div> <div class='ff_full_text ff_hide_text'>{$allText} {$readLessButton}</div>";
+		} else {
 			$innerHtml = $data['settings']['html_codes'];
 		}
 		$html = "<div class='{$cls}' {$atts}>{$innerHtml}</div>";
 		$html = apply_filters('fluentform_rendering_field_html_'.$elementName, $html, $data, $form);
         echo $html;
+		if ($data['settings']['show_as_reveal_text']) {
+			?>
+				<script>
+					(function($){
+						$('.ff_read_more_btn').on('click', function (event) {
+							event.preventDefault();
+
+							var $parent = $(this).closest('.ff-el-group');
+
+							console.log('adre');
+							
+							$parent.find('.ff_truncated_text').hide();
+							$parent.find('.ff_full_text').show();
+						})
+
+						$('.ff_read_less_btn').on('click', function (event) {
+							event.preventDefault();
+
+							var $parent = $(this).closest('.ff-el-group');
+							
+							$parent.find('.ff_truncated_text').show();
+							$parent.find('.ff_full_text').hide();
+
+						})
+					})(jQuery)
+				</script>
+			<?php
+		}
     }
 }
